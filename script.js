a2e1eaf2ee8cfeac37d3f10f1ad5453cfa2069c7
Wright <wrighada@oregonstate.edu>
2019-07-21 16:06:01 -0700

    Update to just weather

--- script.js
+++ script.js
@@ -68,46 +68,4 @@ function bindButtons() {
     // Run once for each button click
     event.preventDefault();
     })
-
-
-    // Attach click handler to http submit form
-    document.getElementById('urlSubmit').addEventListener('click', function(event){
-        let req = new XMLHttpRequest();
-
-        // Receive city from form
-        let post = '';
-        post = document.getElementById('longUrl').value;
-
-        if (post === '') {
-            alert('Please enter a message to be sent');
-        }
-
-        req.open('POST', 'http://httpbin.org/post', true);
-        req.setRequestHeader('Content-Type', 'application/json');
-		req.addEventListener('load', function() {
-			if (req.status >= 200 && req.status < 400) {
-
-                // Log the response
-                console.log(JSON.parse(req.responseText));
-
-                // Pull the value out of the response
-                let response = JSON.parse(req.responseText).json;
-                
-                // Display the entered value
-                document.getElementById('originalUrl').textContent = post;
-
-                // Display the returned value
-                document.getElementById('shortUrl').textContent = response;
-			} else {
-				let str = "Error in network request: " + response.statusText;
-				console.log(str);
-				alert(str);
-            }});
-           
-        // Send the request
-        req.send(JSON.stringify(post));
-        
-        // Run once for each button click
-        event.preventDefault();
-    })
 };
