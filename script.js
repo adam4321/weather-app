fc1745d94e9f34846792ed94b240502a133e00c3
Wright <wrighada@oregonstate.edu>
2019-08-18 05:07:34 -0700

    change to conditional rendering of elements

--- script.js
+++ script.js
@@ -45,22 +45,41 @@ function bindButtons() {
     req.addEventListener('load',function(){
         if (req.status >= 200 && req.status < 400) {
 
+            document.getElementById('form-box').style = 'display:none'
+
             // Log the response
             console.log(JSON.parse(req.responseText));
 
             // Store the response data
             let data = JSON.parse(req.responseText);
 
-            // Populate the current city
-            document.getElementById('city').textContent = ' ' + data.name;
+            let displayBox = document.createElement('div');
+            displayBox.id = 'display-box';
+            document.getElementById('root').appendChild(displayBox);
 
-            // Get the temp and convert from kelvin to fahrenheit
+            // Populate the current city
+            let city = document.createElement('p');
+            city.id = 'city';
+            city.className = 'reply';
+            document.getElementById('display-box').appendChild(city);
+            document.getElementById('city').textContent = 'Location: ' + data.name;
+
+            // Create temp element and convert from kelvin to fahrenheit
+            let tempBox = document.createElement('p');
+            tempBox.id = 'temp';
+            tempBox.className = 'reply';
+            document.getElementById('display-box').appendChild(tempBox);
+            
             let temp = data.main.temp
             temp = (temp - 273.15) * 9/5 + 32
             temp = temp.toPrecision(3);
             document.getElementById('temp').textContent = ' ' + temp + ' degrees F.';
 
             // Show the current conditions
+            let conditions = document.createElement('p');
+            conditions.id = 'other';
+            conditions.className = 'reply';
+            document.getElementById('display-box').appendChild(conditions);
             document.getElementById('other').textContent = ' ' + data.weather[0].description;
 
         } else {
