c24cc6372c852ecc70649771f06211c34af9e8bd
Adam Wright <adam@adamjwright.com>
2020-10-23 14:38:51 -0700

    serve index using express

--- app.js
+++ app.js
@@ -2,17 +2,31 @@
 ** Author:       Adam Wright
 ** Description:  Node webserver that makes calls to the open weather api
 **               and returns the results to the client side weather
-**               widget.
+**               widget. 
 ***********************************************************************/
 
+// Set up express
 let express = require('express');
 let app = express();
+let path = require('path');
 
-// Set static port = 6000
-app.set('port', 6000);
+// Set static port = 6060
+app.set('port', 6060);
+
+// Set up request
+const request = require('request');
+
+// Set up path to static files
+app.use('/weather_app', express.static('public'));
+
+// Root route that serves index.html
+app.get('/weather_app', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+})
 
 
 
+// Start the server on the static port
 app.listen(app.get('port'), function() {
-    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+    console.log(`\nExpress started on http://localhost:${app.get('port')};\npress Ctrl-C to terminate.\n`);
 });
