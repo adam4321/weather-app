088690d36de8fa563b9fdf842a77bd18984ea808
Adam Wright <adam@adamjwright.com>
2020-11-26 13:49:29 -0800

    Clear up flickering initial rendering

--- script.js
+++ script.js
@@ -10,13 +10,6 @@
 import credentials from './credentials.js';
 
 
-// Hide the page's components initially
-document.getElementById('form-header').style = 'display:none';
-document.getElementById('form-box').style = 'display:none';
-document.getElementById('display-box').style = 'display:none';
-document.getElementById('change-btn').style = 'display:none';
-
-
 // Function to match current weather icon to API's suggestion
 let currentIcon;
 
@@ -80,10 +73,17 @@ function findIcon(data) {
 }
 
 
+// Button so the user can change location
+document.getElementById('change-btn').addEventListener('click', (event) => {
+    location = '';
+    showForm();
+})
+
+
 // Helper function to display the city select form and hide current weather box
 function showForm() {
     document.getElementById('input-form').reset();
-    document.getElementById('form-box').style = '';
+    document.getElementById('form-box').style.display = 'block';
     document.getElementById('text-box').focus();
     document.getElementById('display-box').style = 'display:none';
     document.getElementById('change-btn').style = 'display:none';
@@ -99,16 +99,16 @@ let span1 = document.getElementsByClassName("close")[0];
 let span2 = document.getElementsByClassName("close")[1];
 
 // When the user clicks on <span> (x), close the modal
-span1.onclick = function() {
+span1.onclick = () => {
     emptyModal.style.display = "none";
 }
 
-span2.onclick = function() {
+span2.onclick = () => {
     notFoundModal.style.display = "none";
 }
   
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = (event) => {
     if (event.target == emptyModal || event.target == notFoundModal) {
         emptyModal.style.display = "none";
         notFoundModal.style.display = "none";
@@ -121,38 +121,32 @@ const locationReq = new XMLHttpRequest();
 let location;
 
 locationReq.open('GET', 'https://geoip-db.com/json/', true);
-locationReq.onload = function() {
+locationReq.onload = () => {
     if (locationReq.status >= 200 && locationReq.status < 400) {
         const locationData = JSON.parse(locationReq.responseText);
 
-        // // Log the responses from the API for testing
-        // console.log(locationData.country_name);
-        // console.log(locationData.state);
-        // console.log(locationData.city);
-        // console.log(locationData.latitude);
-        // console.log(locationData.longitude);
-        // console.log(locationData.IPv4);
-
+        // Attempt to find the weather if the city was found
         if (locationData.city) {
             location = locationData.city;
             document.getElementById('city-submit').click();
         }
+        // We reached our target server, but it returned no city
         else {
-            // We reached our target server, but it returned no city
-            console.log('Error from geolocation service')
+            console.error('Error from geolocation service')
             showForm();
         }
     }
+    // We reached our target server, but it returned an error
     else {
-        // We reached our target server, but it returned an error
-        console.log('Error from geolocation service')
+        console.error('Error from geolocation service')
         showForm();
     }
 };
 
-locationReq.onerror = function() {
-    // There was a connection error of some sort
-    console.log('Geolocation connection error')
+
+// There was a connection error of some sort
+locationReq.onerror = () => {
+    console.error('Geolocation connection error')
     showForm();
 };
 
@@ -162,7 +156,7 @@ locationReq.send();
 // Populate weather if geolocation worked or render the choose city form
 const req = new XMLHttpRequest();
 
-document.getElementById('city-submit').addEventListener('click', function(event) {
+document.getElementById('city-submit').addEventListener('click', (event) => {
 
     // Receive city from form
     let city = document.getElementById('text-box').value;
@@ -186,11 +180,8 @@ document.getElementById('city-submit').addEventListener('click', function(event)
 
     // Open get request to the open weather api
     req.open('GET', `https://api.openweathermap.org/data/2.5/weather?q=${location},us&APPID=${credentials}`, true);
-    req.addEventListener('load',function(){
+    req.addEventListener('load', () => {
         if (req.status >= 200 && req.status < 400) {
-
-            // // Log the response for testing
-            // console.log(JSON.parse(req.responseText));
 
             // Store the response data
             const data = JSON.parse(req.responseText);
@@ -214,15 +205,15 @@ document.getElementById('city-submit').addEventListener('click', function(event)
             document.getElementById('temp').textContent = `${temp.val}\xB0 ${temp.tempType}`;
 
             // Farenheit or Celcius button
-            document.getElementById('standard-btn').addEventListener('click', function(event) {
+            document.getElementById('standard-btn').addEventListener('click', (event) => {
                 if (temp.tempType == 'F') {
                     temp.tempType = 'C';
-                    temp.val = data.main.temp
+                    temp.val = data.main.temp;
                     temp.val = temp.val - 273.15;
                 }
                 else if (temp.tempType == 'C') {
                     temp.tempType = 'F';
-                    temp.val = data.main.temp
+                    temp.val = data.main.temp;
                     temp.val = (temp.val - 273.15) * 9/5 + 32;
                 }
                 temp.val = Math.floor(temp.val);
@@ -230,15 +221,9 @@ document.getElementById('city-submit').addEventListener('click', function(event)
             })
 
             // Show the weather result and change button
-            document.getElementById('display-box').style = ''
+            document.getElementById('display-box').style = '';
             document.getElementById('change-btn').style = '';
             city = '';
-
-            // Button so user can change location
-            document.getElementById('change-btn').addEventListener('click', function(event) {
-                location = '';
-                showForm();
-            })
         } 
         else {
             console.log('Error in weather API request: ' + req.statusText);
